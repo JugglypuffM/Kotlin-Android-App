0b2b2f19808d536f90d01a4050fa288cdf9ea004
Jut1S <vip.nikita.bulygin@gmail.com>
2024-10-12 18:35:01 +0500

    Добавление: Экран авторизации

---
+++ app/src/main/java/com/example/KotlinAndroidApp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.KotlinAndroidApp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlin_android_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val label = findViewById<TextView>(R.id.main_label)
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userPassword: EditText = findViewById(R.id.user_password)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener{
+            val text = userLogin.text.toString().trim()
+            label.text = text
+        }
+
+    }
+}
