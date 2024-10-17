dc08561fb8b4d548910225ffacfb01361015b432
Nikita <vip.nikita.bulygin@gmail.com>
2024-10-18 01:56:27 +0500

    Добавление окна регистрации и автоматизация кнопок для перехода на разные экраны

--- app/src/main/kotlin/KotlinAndroidApp/MainActivity.kt
+++ app/src/main/kotlin/KotlinAndroidApp/MainActivity.kt
@@ -1,34 +1,26 @@
 package KotlinAndroidApp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.project.kotlin_android_app.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val label = findViewById<TextView>(R.id.main_label)
-        val userLogin: EditText = findViewById(R.id.user_login)
         val userPassword: EditText = findViewById(R.id.user_password)
-        val button: Button = findViewById(R.id.button)
 
-        button.setOnClickListener{
-            val text = userLogin.text.toString().trim()
-            label.text = text
+        val buttonRegister: Button = findViewById(R.id.button_register)
+
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
         }
 
     }
