f8203e91af9c80c0b721b2046b06bfdb89d1bdc6
Nikita <vip.nikita.bulygin@gmail.com>
2024-10-18 18:42:50 +0500

    Добавление возможности базовой регистрации и авторизации. При авторизации открывается окно с данными о пользователе

--- app/src/main/kotlin/KotlinAndroidApp/MainActivity.kt
+++ app/src/main/kotlin/KotlinAndroidApp/MainActivity.kt
@@ -2,9 +2,10 @@ package KotlinAndroidApp
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.project.kotlin_android_app.R
 
@@ -14,14 +15,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userPassword: EditText = findViewById(R.id.user_password)
+        val loginField: EditText = findViewById(R.id.user_login)
+        val passwordField: EditText = findViewById(R.id.user_password)
+        val loginButton: Button = findViewById(R.id.button)
+        val textViewRegister: TextView = findViewById(R.id.textview_register)
 
-        val buttonRegister: Button = findViewById(R.id.button_register)
+        val registeredLogin = intent.getStringExtra("EXTRA_LOGIN")
+        val registeredPassword = intent.getStringExtra("EXTRA_PASSWORD")
+        val registeredName = intent.getStringExtra("EXTRA_NAME")
 
-        buttonRegister.setOnClickListener {
+        textViewRegister.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
 
+        loginButton.setOnClickListener {
+            val inputLogin = loginField.text.toString()
+            val inputPassword = passwordField.text.toString()
+
+            if (inputLogin == registeredLogin && inputPassword == registeredPassword) {
+
+                val intent = Intent(this, UserProfileActivity::class.java).apply {
+                    putExtra("EXTRA_LOGIN", registeredLogin)
+                    putExtra("EXTRA_NAME", registeredName)
+                }
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Неверный логин или пароль", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/kotlin/KotlinAndroidApp/RegistrationActivity.kt
+++ app/src/main/kotlin/KotlinAndroidApp/RegistrationActivity.kt
@@ -2,7 +2,11 @@ package KotlinAndroidApp
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.project.kotlin_android_app.R
 
@@ -11,14 +15,38 @@ class RegistrationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-
         val textViewLogin: TextView = findViewById(R.id.textview_login)
+        val registerButton: Button = findViewById(R.id.registration_button)
+        val nameField: EditText = findViewById(R.id.user_login3)
+        val loginField: EditText = findViewById(R.id.user_login2)
+        val passwordField: EditText = findViewById(R.id.user_password2)
+        val confirmPasswordField: EditText = findViewById(R.id.user_password3)
 
         textViewLogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        registerButton.setOnClickListener {
+            val name = nameField.text.toString()
+            val login = loginField.text.toString()
+            val password = passwordField.text.toString()
+            val confirmPassword = confirmPasswordField.text.toString()
+
+            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(login) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_SHORT).show()
+            } else if (password != confirmPassword) {
+                Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Успешная регистрация", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    putExtra("EXTRA_NAME", name)
+                    putExtra("EXTRA_LOGIN", login)
+                    putExtra("EXTRA_PASSWORD", password)
+                }
+                startActivity(intent)
+            }
+        }
     }
-
-
 }
